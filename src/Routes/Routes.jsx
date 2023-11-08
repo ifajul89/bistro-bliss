4fff88a408d5acb06468616dddc017478e52e482
Blue Bird <bluebird089@proton.me>
2023-11-08 12:21:46 +0600

    added tailwind, daisyui, react-router, ffirebase

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "../Pages/Error/ErrorPage";
+import Home from "../Pages/Home/Home";
+import Root from "../Layout/Root";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>,
+            },
+        ],
+    },
+]);
+
+export default router;
