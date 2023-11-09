0d6747df9305a98c0a1fce918e63db6bbf787bfe
Blue Bird <bluebird089@proton.me>
2023-11-09 19:14:03 +0600

    added registration page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import "./Register.css";
 import { AuthContext } from "../../Provider/AuthProvider";
+import RegisterBg from "../../assets/register.jpg";
 
 const Register = () => {
     const { createUser } = useContext(AuthContext);
@@ -24,59 +25,89 @@ const Register = () => {
     };
 
     return (
-        <div className="register-bg">
-            <div className="bg-gradient-to-r from-white to-transparent">
-                <form onSubmit={handleRegister} className="bg-white w-[400px]">
+        <div className="container mx-auto flex gap-10 p-10">
+            <div className="border-4 p-10 flex flex-col items-center gap-5 rounded-3xl w-1/3">
+                <div className="w-2/3">
+                    <img className="rounded-full" src={RegisterBg} alt="" />
+                </div>
+                <h3 className="font-cursive font-light text-lg text-gray-500">
+                    Hello User,
+                </h3>
+                <h4 className="font-cursive font bold text-4xl text-center">
+                    Please Register
+                </h4>
+                <h4 className="font-cursive font bold text-4xl text-center text-[#F2A64D]">
+                    Bon Appétit
+                </h4>
+            </div>
+            <div className="bg-white border-4 w-2/3 rounded-3xl p-10">
+                <form onSubmit={handleRegister} className="">
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Name</span>
+                            <span className="label-text text-gray-600">
+                                Name
+                            </span>
                         </label>
                         <input
                             type="text"
                             name="name"
                             placeholder="Type Your Name"
-                            className="input input-bordered"
+                            className="input rounded-full focus:outline-none bg-gray-100"
                             required
                         />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Photo</span>
+                            <span className="label-text text-gray-600">
+                                Photo
+                            </span>
                         </label>
                         <input
                             type="text"
                             name="photo"
                             placeholder="Enter Photo URL"
-                            className="input input-bordered"
+                            className="input rounded-full focus:outline-none bg-gray-100"
                             required
                         />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Email</span>
+                            <span className="label-text text-gray-600">
+                                Email
+                            </span>
                         </label>
                         <input
                             type="email"
                             name="email"
                             placeholder="Type Your Email"
-                            className="input input-bordered"
+                            className="input rounded-full focus:outline-none bg-gray-100"
                             required
                         />
                     </div>
                     <div className="form-control">
                         <label className="label">
-                            <span className="label-text">Password</span>
+                            <span className="label-text text-gray-600">
+                                Password
+                            </span>
                         </label>
                         <input
                             type="password"
                             name="password"
                             placeholder="Type Your Password"
-                            className="input input-bordered"
+                            className="input rounded-full focus:outline-none bg-gray-100"
                             required
                         />
                     </div>
+                    <div className="mt-4 flex items-center gap-1">
+                        <input type="checkbox" name="" id="" />
+                        <h3>I Accept The <a className="underline text-[#F2A64D]" href="#">Term and Conditions</a></h3>
+                    </div>
                     <div className="form-control mt-6">
-                        <input className="btn" type="submit" value="Register" />
+                        <input
+                            className="btn rounded-full bg-[#F2A64D] text-white border-0 hover:bg-[#d68a34]"
+                            type="submit"
+                            value="Register"
+                        />
                     </div>
                 </form>
             </div>
