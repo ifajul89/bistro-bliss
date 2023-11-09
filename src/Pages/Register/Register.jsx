cc1a31a6c9a6cf964d78dd130945fb94c0c9a02b
Blue Bird <bluebird089@proton.me>
2023-11-09 20:14:24 +0600

    added responsive design for register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -14,8 +14,6 @@ const Register = () => {
         const password = form.password.value;
         console.log(name, photo, email, password);
 
-
-
         createUser(email, password)
             .then((result) => {
                 console.log(result);
@@ -26,22 +24,22 @@ const Register = () => {
     };
 
     return (
-        <div className="container mx-auto flex gap-10 p-10">
-            <div className="border-4 p-10 flex flex-col items-center gap-5 rounded-3xl w-1/3">
-                <div className="w-2/3">
+        <div className="container mx-auto flex flex-col mb-5 md:flex-row gap-3 md:gap-5 lg:gap-10 lg:p-10 px-3 md:px-0">
+            <div className="border-4 p-2 md:p-5 lg:p-10 flex justify-between md:justify-start md:flex-col items-center gap-3 md:gap-5 rounded-3xl md:w-1/3">
+                <div className="w-[44px] sm:w-[70px] md:w-[170px] lg:w-[250px]">
                     <img className="rounded-full" src={RegisterBg} alt="" />
                 </div>
-                <h3 className="font-cursive font-light text-lg text-gray-500">
+                <h3 className="font-cursive hidden md:inline-block font-light text-lg text-gray-500">
                     Hello User,
                 </h3>
-                <h4 className="font-cursive font bold text-4xl text-center">
+                <h4 className="font-cursive font bold text-2xl lg:text-4xl text-center">
                     Please Register
                 </h4>
-                <h4 className="font-cursive font bold text-4xl text-center text-[#F2A64D]">
+                <h4 className="font-cursive font bold text-2xl lg:text-4xl text-center text-[#F2A64D]">
                     Bon Appétit
                 </h4>
             </div>
-            <div className="bg-white border-4 w-2/3 rounded-3xl p-10">
+            <div className="bg-white border-4 md:w-2/3 rounded-3xl p-3 md:p-5 lg:p-10">
                 <form onSubmit={handleRegister} className="">
                     <div className="form-control">
                         <label className="label">
@@ -101,7 +99,12 @@ const Register = () => {
                     </div>
                     <div className="mt-4 flex items-center gap-1">
                         <input type="checkbox" name="" id="" />
-                        <h3>I Accept The <a className="underline text-[#F2A64D]" href="#">Term and Conditions</a></h3>
+                        <h3>
+                            I Accept The{" "}
+                            <a className="underline text-[#F2A64D]" href="#">
+                                Term and Conditions
+                            </a>
+                        </h3>
                     </div>
                     <div className="form-control mt-6">
                         <input
