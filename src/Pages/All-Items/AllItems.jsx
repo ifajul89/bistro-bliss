0452f24ba6404b5b3e5fcc1e582f5f9a096d61c8
Blue Bird <bluebird089@proton.me>
2023-11-12 20:27:09 +0600

    added minor search featur

--- src/Pages/All-Items/AllItems.jsx
+++ src/Pages/All-Items/AllItems.jsx
@@ -1,59 +1,53 @@
-import { useQuery } from "@tanstack/react-query";
 import SingleItem from "./SingleItem/SingleItem";
 // import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import { useState } from "react";
-// import { useEffect, useState } from "react";
-// import { useState } from "react";
-// import { useEffect } from "react";
-// import axios from "axios";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const AllItems = () => {
     // const [dataCount, setDataCount] = useState(0);
 
     const { count } = useLoaderData();
     const [currentPage, setCurrentPage] = useState(1);
-    // const [foods, setFoods] = useState([]);
+    const [foods, setFoods] = useState([]);
+    const [search, setSearch] = useState("");
 
     const itemsPerPage = 9;
     const numberOfPages = Math.ceil(count / itemsPerPage);
     const pagesNumber = [...Array(numberOfPages).keys()];
 
-    const { isPending, data: foods } = useQuery({
-        queryKey: ["foods"],
-        queryFn: async () => {
-            const res = await fetch(
-                `http://localhost:5000/foods?page=${currentPage}&size=${itemsPerPage}`
-            );
-            return res.json();
-        },
-    });
-
-    // useEffect(() => {
-    //     fetch(
-    //         `http://localhost:5000/foods?page=${currentPage}&size=${itemsPerPage}`
-    //     )
-    //         .then((res) => res.json())
-    //         .then((data) => setFoods(data));
-    // }, [currentPage, itemsPerPage]);
+    useEffect(() => {
+        axios.get( `http://localhost:5000/foods?search=${search}&page=${currentPage}&size=${itemsPerPage}`)
+        .then(data => {
+            setFoods(data.data)
+        })
+    }, [search, currentPage, itemsPerPage])
 
     const handleSetCurrentPage = (pageNumber) => {
         setCurrentPage(pageNumber);
     };
 
-    if (isPending) {
-        return (
-            <div className="flex justify-center my-40">
-                <span className="loading text-[#F2A64D] loading-dots loading-lg"></span>
-            </div>
-        );
-    }
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const searchFieldValue = e.target.search.value;
+        setSearch(searchFieldValue);
+    };
 
     return (
         <div className="container mx-auto px-3 md:px-0">
             <h3 className="py-3 px-4  md:text-base lg:text-lg font-cursive font-semibold bg-white rounded-2xl border-4 text-center ">
                 ALL ITEMS
             </h3>
+            <form onSubmit={handleSearch} className="flex">
+                <div className="form-control">
+                    <input
+                        type="text"
+                        name="search"
+                        className="input rounded-full focus:outline-none bg-gray-100"
+                    />
+                </div>
+                <input className="btn" type="submit" value="Search" />
+            </form>
             <div className="grid md:grid-cols-3 gap-5 my-5">
                 {foods.map((food) => (
                     <SingleItem key={food._id} food={food}></SingleItem>
--- src/Pages/PurchasePage/PurchasePage.jsx
+++ src/Pages/PurchasePage/PurchasePage.jsx
@@ -1,37 +1,44 @@
-// import { useLoaderData } from "react-router-dom";
+import { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const PurchasePage = () => {
-    
-    // const {
-    //     _id,
-    //     image,
-    //     foodName,
-    //     category,
-    //     price,
-    //     madeBy,
-    //     foodOrigin,
-    //     shortDescription,
-    // } = useLoaderData();
+    const { user } = useContext(AuthContext);
+
+    const {
+        image,
+        foodName,
+        category,
+        price,
+        madeBy,
+        foodOrigin,
+        shortDescription,
+    } = useLoaderData();
 
     return (
-        <div>
-            <form>
-                <div className="form-control">
-                    <label className="label">
-                        <span className="label-text text-gray-600">
-                            Food Name
-                        </span>
-                    </label>
-                    <input
-                        type="text"
-                        name="foodName"
-                        id="readOnlyField"
-                        // value={foodName}
-                        className="input rounded-full focus:outline-none bg-gray-100"
-                        required
-                    />
-                </div>
-            </form>
+        <div className="container mx-auto grid grid-cols-3">
+            <div className="col-span-1">
+                <img src={image} alt="" />
+            </div>
+            <div className="col-span-2">
+                <form>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text text-gray-600">
+                                Food Name
+                            </span>
+                        </label>
+                        <input
+                            type="text"
+                            name="foodName"
+                            value={foodName}
+                            className="input rounded-full focus:outline-none bg-gray-100"
+                            readOnly
+                            required
+                        />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
